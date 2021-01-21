02078ecc6d69b002fd21aeec2f68eb59b217dc14
kenza <tf.kenza@gmail.com>
2021-01-21 18:38:42 +0100

    deuxième commit

---
+++ src/main/java/ma/uit/TP/App.java
@@ -0,0 +1,15 @@
+package ma.uit.TP;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("bonjour");
+    }
+}
